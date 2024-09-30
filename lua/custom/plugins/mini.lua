2bc9054686577bb328ba9ef11a04158f18f100ff
Sid <sidoniofcam@gmail.com>
2024-09-30 19:40:39 +0100

    configure status line info

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -20,6 +20,7 @@ return {
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
+
     local statusline = require 'mini.statusline'
     -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
@@ -31,6 +32,28 @@ return {
     statusline.section_location = function()
       return '%2l:%-2v'
     end
+
+    require('mini.statusline').setup {
+      function()
+        local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+        local git = MiniStatusline.section_git { trunc_width = 40 }
+        local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+        local filename = MiniStatusline.section_filename { trunc_width = 140 }
+        local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+        local location = MiniStatusline.section_location { trunc_width = 75 }
+        local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+        return MiniStatusline.combine_groups {
+          { hl = mode_hl, strings = { mode } },
+          { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+          '%<', -- Mark general truncate point
+          { hl = 'MiniStatuslineFilename', strings = { filename } },
+          '%=', -- End left alignment
+          { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+          { hl = mode_hl, strings = { search, location } },
+        }
+      end,
+    }
 
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
